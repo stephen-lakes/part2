d7be7ad2fa159a8314e5f5a47bc423dfd60cc435
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-04 07:13:01 +0100

    set token on successful login

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,7 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password });
 
+      noteService.setToken(user.token);
       setUser(user);
       setUsername("");
       setPassword("");
