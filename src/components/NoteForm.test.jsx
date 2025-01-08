19f3cccada64dc6aeb518df28a0e6979a9a8070e
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-08 11:37:00 +0100

    get input using queryselector
    destructure container fromn render function

--- src/components/NoteForm.test.jsx
+++ src/components/NoteForm.test.jsx
@@ -7,9 +7,9 @@ test("<NoteForm /> updates parent state and calls onSubmit", async () => {
   const createNote = vi.fn();
   const user = userEvent.setup();
 
-  render(<NoteForm createNote={createNote} />);
+  const { container } = render(<NoteForm createNote={createNote} />);
 
-  const input = screen.getByPlaceholderText("write note content here");
+  const input = container.querySelector("#note-input");
   const sendButton = screen.getByText("save");
 
   await user.type(input, "testing a form...");
